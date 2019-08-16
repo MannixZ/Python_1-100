e0671094344070b09d724de32f8de81636066be7
Mannix <317588213@qq.com>
2019-08-16 10:28:11 +0800

    update

--- day10/game_window.py
+++ day10/game_window.py
@@ -15,12 +15,8 @@ def main():
     screen = pygame.display.set_mode((800, 600))
     # 设置当前窗口的标题
     pygame.display.set_caption('大球吃小球')
-    # 设置窗口的背景色(颜色是由红绿蓝三原色构成的元祖)
-    screen.fill((242, 242, 242))
-    # 绘制一个圆(参数分别是:屏幕, 颜色, 圆心位置, 半径, 0表示填充圆)
-    pygame.draw.circle(screen, (255, 0, 0), (100, 100), 30, 0)
-    # 刷新当前窗口(渲染窗口将绘制的图像呈现出来)
-    pygame.display.flip()
+    # 顶一变量来表示小球在屏幕上的位置
+    x, y = 50, 50
     running = True
     # 开启一个事件循环处理发生的事件
     while running:
@@ -28,6 +24,29 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        screen.fill((255, 255, 255))
+        pygame.draw.circle(screen, (255, 0, 0), (x, y), 30, 0)
+        pygame.display.flip()
+        # 每隔50毫秒就改变小球的位置再刷新窗口
+        pygame.time.delay(50)
+        x, y = x + 5, y + 5
+    # # 设置窗口的背景色(颜色是由红绿蓝三原色构成的元祖)
+    # screen.fill((255, 255, 255))
+    # # 通过制定的文件名加载图像
+    # ball_image = pygame.image.load('./images/ball.png')
+    # # # 绘制一个圆(参数分别是:屏幕, 颜色, 圆心位置, 半径, 0表示填充圆)
+    # # pygame.draw.circle(screen, (255, 0, 0), (100, 100), 30, 0)
+    # # 在窗口上渲染图像
+    # screen.blit(ball_image, (50, 50))
+    # # 刷新当前窗口(渲染窗口将绘制的图像呈现出来)
+    # pygame.display.flip()
+    # running = True
+    # # 开启一个事件循环处理发生的事件
+    # while running:
+    #     # 从消息队列中获取事件并对事件进行处理
+    #     for event in pygame.event.get():
+    #         if event.type == pygame.QUIT:
+    #             running = False
 
 if __name__ == '__main__':
     main()
