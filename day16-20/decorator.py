4124d8f6bbda2c2b4558882529e9950912c994cf
Mannix <317588213@qq.com>
2019-09-26 09:36:07 +0800

    update

--- day16-20/decorator.py
+++ day16-20/decorator.py
@@ -40,3 +40,68 @@ def record(output):
         return wrapper()
 
     return decorate
+
+
+"""自定义装饰器类(通过__call__魔术方法使得对象可以当成函数调用)"""
+class Record():
+
+    def __init__(self, output):
+        self.output = output
+
+    def __call__(self, func):
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            start = time()
+            result = func(*args, **kwargs)
+            self.output(func.__name__, time() - start)
+            return result
+
+        return wrapper
+
+
+'''
+例子：用装饰器来实现单例模式。
+'''
+from functools import wraps
+
+
+def singleton(cls):
+    '''装饰类的装饰器'''
+    instances = {}
+
+    @wraps(cls)
+    def wrapper(*args, **kwargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+
+    return wrapper()
+
+
+@singleton
+class President()
+    '''总统(单例类)'''
+    pass
+
+
+'''
+实现线程安全的单例
+'''
+from functools import wraps
+from threading import Lock
+
+def singleton(cls):
+    '''线程安全的单例装饰器'''
+    instance = {}
+    locker = Lock()
+
+    @wraps(cls)
+    def wrapper(*args, **kwargs):
+        if cls not in instance:
+            with locker:
+                if cls not in instance:
+                    instance[cls] = cls(*args, **kwargs)
+        return instance[cls]
+
+    return wrapper()
